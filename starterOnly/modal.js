45c6bf855344d610778fec75a1958287fba419f2
Ulysse37 <ulysse.courtet@hotmail.fr>
2021-07-28 11:04:47 +0200

    Issue 1 : fermer la modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,26 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-
+const modalHide = document.querySelectorAll(".close");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Close modal event
+modalHide.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
 
+// Echap close  ------> Pourquoi elle ne marche pas ? 
+function keyPress (e) {
+  if(e.key === "Escape") {
+      closeModal();
+  }
+}
