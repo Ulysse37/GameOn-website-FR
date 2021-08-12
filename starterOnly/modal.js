19db17d2800def55a198701716c02f4238ed8e68
Ulysse37 <ulysse.courtet@hotmail.fr>
2021-08-12 08:40:35 +0200

    Issue numéro 2

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,17 @@
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const modalHide = document.querySelectorAll(".close");
+// const qui vont chercher les input à tester
+const first = document.getElementById("first");
+const last = document.getElementById("last");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+// const location = document.getElementByName("location"); -> bloque tout le site
+const condition = document.getElementById("checkbox1");
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -6,18 +20,6 @@ function editNav() {
     x.className = "topnav";
   }
 }
-
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const modalHide = document.querySelectorAll(".close");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// Close modal event
-modalHide.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
@@ -29,52 +31,83 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+
 // Ckeck si les données du formulaire sont valides
 function validationPrenom() {
-  if ((first.length >= 2) & (first != "")) {
-  return true;
-  } else {
-    return false;
-  }
+
+  if ((first.value.length >= 2) & (first.value != "")) {
+
+    return true;
+  } 
+
+  return false;
 }
 
 function validationNom() {
-  if ((last.length >= 2) & (last != "")) {
-  return true;
-  } else {
-    return false;
-  }
+
+  if ((last.value.length >= 2) & (last.value != "")) {
+
+    return true;
+  } 
+
+  return false; 
 }
 
 function validationEmail() {
   let validMail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
   if (email.value.match(validMail)) {
+    
     return true;
-  } else {
-    return false;
-  }
+  } 
+  
+  return false;
 }
 
+
+//console.log(quantity.value);
 function validationQté() {
-  if (isNaN(quantity)) {
+
+  if ((isNaN(quantity.value)) & (quantity.value == "")) {
+
     return false;
-  } else {
-    return true;
-  }
+  } 
+
+  return true;
 }
+//console.log(validationQté());
 
 function validationBouton() {
-  if (document.getElementsByName("location").checked) {
+
+  if ((document.getElementById("location1").checked) || (document.getElementById("location2").checked) || (document.getElementById("location3").checked)
+   || (document.getElementById("location4").checked) || (document.getElementById("location5").checked) || (document.getElementById("location6").checked)) {
+
     return true;
-  } else {
-    return false;
-  }
+  } 
+
+  return false;
 }
 
 function validationCondition() {
-  if (document.getElementById("checkbox1").checked) {
+
+  if (condition.checked) {
+
     return true;
-  } else {
-    return false;
-  }
+  } 
+
+  return false;
 }
+
+
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Close modal event
+modalHide.forEach((btn) => btn.addEventListener("click", closeModal));
+
+
+
+
+
+
